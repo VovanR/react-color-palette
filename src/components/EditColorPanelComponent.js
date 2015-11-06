136c16d3dc3583c2b4dfab6fd960fac5ebf0a951
Vladimir Rodkin <mail@vovanr.com>
2015-11-06 22:23:38 +0300

    EditColorPanel: Add scrollbar

--- src/components/EditColorPanelComponent.js
+++ src/components/EditColorPanelComponent.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import SelectedColorComponent from './SelectedColorComponent';
 import { Panel, Badge } from 'react-bootstrap';
+import ScrollArea from 'react-scrollbar';
 
 require('styles/EditColorPanel.css');
 
@@ -25,24 +26,25 @@ class EditColorPanelComponent extends React.Component {
 		return (
 			<div className="editcolorpanel-component">
 				<Panel header={header}>
-					<div className="palette-selected-colors-placeholder">
-						<div className="palette-selected-colors__scrollbar">
-							<div className="palette-selected-colors">
-								{this.props.colors.map(color => {
-									return (
-										<SelectedColorComponent
-											key={color.id}
-											color={color}
-											onChange={this.handleChangeColor.bind(this)}
-											onDelete={this.handleDeleteColor.bind(this)}
-											onMouseEnter={this.props.onMouseEnter.bind(this, color.id)}
-											onMouseLeave={this.props.onMouseLeave.bind(this, color.id)}
-										/>
-									);
-								})}
-							</div>
-						</div>
-					</div>
+					<ScrollArea
+						speed={0.8}
+						className="editcolorpanel-component-placeholder"
+						contentClassName="palette-selected-colors"
+						horizontal={false}
+					>
+						{this.props.colors.map(color => {
+							return (
+								<SelectedColorComponent
+									key={color.id}
+									color={color}
+									onChange={this.handleChangeColor.bind(this)}
+									onDelete={this.handleDeleteColor.bind(this)}
+									onMouseEnter={this.props.onMouseEnter.bind(this, color.id)}
+									onMouseLeave={this.props.onMouseLeave.bind(this, color.id)}
+								/>
+							);
+						})}
+					</ScrollArea>
 				</Panel>
 			</div>
 		);
